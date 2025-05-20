068b2eb154761ec7aea71c95a254a51888a3000f
fincatomarta <fincatomarta16@gmail.com>
2025-05-20 14:20:46 +0000

    Esempio 3

---
+++ example3/build/CMakeFiles/summer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/summer.dir/summer.cpp.o"
+  "CMakeFiles/summer.dir/summer.cpp.o.d"
+  "summer"
+  "summer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/summer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ example3/build/CMakeFiles/summer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for summer.
+# This may be replaced when dependencies are built.
